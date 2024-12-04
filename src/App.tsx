9369e6f7a9bd32f81b3e1d99376561ec9e62a327
Oscarmrl <oscaralexandermurillo500@gmail.com>
2024-12-03 21:54:36 -0600

    Configuracion inicial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className=" text-center text-4xl text-red-900 justify-center justify-items-center">
+        Hola mundo
+      </div>
+    </>
+  );
+}
+
+export default App;
