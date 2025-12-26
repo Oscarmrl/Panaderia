a2ee70a1a3c0a1a4b92df7b7d47a2d68493e8543
Oscar-Murillo <oscaralexandermurillo500@gmail.com>
2025-12-26 00:48:02 -0600

    feat: :zap: Pruebas iniciales de renderizado condicional

--- src/Components/Footer/FooterMenu.tsx
+++ src/Components/Footer/FooterMenu.tsx
@@ -4,10 +4,13 @@ import IconoNav from "../ui/IconoNav";
 import { IoHomeOutline } from "react-icons/io5";
 import useNavigation from "../../hook/useNavigation";
 import { useNavigationContext } from "../../context/useNavigationContext";
-
+import { MdAdminPanelSettings } from "react-icons/md";
+import { getAuthUser } from "../../auth/authStorage";
 export default function MenuInferior() {
-  const { goToCart, gotToHome, goToFavorites } = useNavigation();
+  const { goToCart, gotToHome, goToFavorites, goToAdminDashboard } =
+    useNavigation();
   const { activeTab } = useNavigationContext();
+  const { role } = getAuthUser();
 
   return (
     <div className="flex flex-row bg-base-100 bottom-0 fixed w-full justify-evenly py-2 md:hidden">
@@ -46,6 +49,20 @@ export default function MenuInferior() {
       >
         <IconoNav icon={MdOutlineShoppingCart} size="w-4 h-4" label="Carrito" />
       </button>
+      {/* BOTÓN ADMIN */}
+      {role === "admin" && (
+        <button
+          data-tab="admin"
+          className={
+            activeTab === "admin"
+              ? "Menu_active"
+              : "flex flex-col items-center justify-center w-20 md:w-40"
+          }
+          onClick={goToAdminDashboard}
+        >
+          <IconoNav icon={MdAdminPanelSettings} size="w-4 h-4" label="Admin" />
+        </button>
+      )}
     </div>
   );
 }
