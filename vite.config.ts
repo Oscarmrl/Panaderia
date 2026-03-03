2802aa1d4f621a4b65b14ae55d619ec5675b26eb
Oscar-Murillo <oscaralexandermurillo500@gmail.com>
2026-03-02 22:41:26 -0600

    feat: :bug: Configuracion para desplegar en vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { readFileSync } from "fs";
-import { join } from "path";
-import { fileURLToPath } from "url";
-
-// Get directory name
-const __dirname = fileURLToPath(new URL(".", import.meta.url));
-
-// Read package.json to get homepage field
-const packageJsonPath = join(__dirname, "package.json");
-const packageJson = JSON.parse(readFileSync(packageJsonPath, "utf-8"));
-const homepage = packageJson.homepage;
-
-// Determine base URL: env variable > homepage > default "/"
-const base = process.env.VITE_BASE_URL || homepage || "/";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base,
 });
